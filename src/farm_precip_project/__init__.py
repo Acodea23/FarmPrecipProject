4eb5b65499c8636c8e0346acb18ac1180490ca44
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-05 15:25:20 -0700

    package now integrates with farm data scraping

--- src/farm_precip_project/__init__.py
+++ src/farm_precip_project/__init__.py
@@ -1,7 +1,7 @@
 from .scrape_precip import write_txt, txt_to_csv, read_url_txt
-from .scrape_farm import farm_test
+from .scrape_farm import row_by_label, extract_state_rows, scrape_farm_data
 # import from all .py coding
 # all code should be in this folder
 # uv pip install -e
 
-all = ["write_txt", "txt_to_csv", "read_url_txt", "extract_state_rows"]
+all = ["write_txt", "txt_to_csv", "read_url_txt", "row_by_label", "extract_state_rows", "scrape_farm_data"]
