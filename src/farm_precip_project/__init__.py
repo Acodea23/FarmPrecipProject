a36082695be7f0012f73bea3390e153bf6cd1010
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-05 14:42:34 -0700

    working package

--- src/farm_precip_project/__init__.py
+++ src/farm_precip_project/__init__.py
@@ -1,4 +1,7 @@
-from scrape_precip import write_txt
+from .scrape_precip import write_txt, txt_to_csv, read_url_txt
+from .scrape_farm import farm_test
 # import from all .py coding
 # all code should be in this folder
+# uv pip install -e
 
+all = ["write_txt", "txt_to_csv", "read_url_txt", "extract_state_rows"]
