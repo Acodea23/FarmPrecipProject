89acbdf71ce5afe7cb72a718de87afe1f82156ca
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-10 13:52:41 -0700

    added merging functions for csvs

--- src/farm_precip_project/__init__.py
+++ src/farm_precip_project/__init__.py
@@ -1,11 +1,15 @@
 from .scrape_precip import txt_to_csv, read_url_txt, normalized_data
 from .scrape_farm import row_by_label, extract_state_rows, scrape_farm_data
 from .eda_work import basic_summary, precip_trend_figure, crop_income_fig, precip_v_income, statcompscatt, correl, heatmap
+from .analysis import analysis_test
+from .merge_csvs import merge_csvs
 # import from all .py coding
 # all code should be in this folder
 # uv pip install -e
 
 all = ["txt_to_csv", "read_url_txt", "normalized_data", 
        "row_by_label", "extract_state_rows", "scrape_farm_data", 
-       "basic_summary", "precip_trend_figure", "crop_income_fig", "precip_v_income", "statcompscatt", "correl", "heatmap"
+       "basic_summary", "precip_trend_figure", "crop_income_fig", "precip_v_income", "statcompscatt", "correl", "heatmap",
+       "analysis_test",
+       "merge_csvs"
        ]
