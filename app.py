0ac459136ee49782cf7bf1493a9db049d4d46a07
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-10 12:02:20 -0700

    saved changes to streamlit app inital set up

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 import pandas as pd
 
 st.title("State Farm Precipitation and Income Data Analysis")
+st.subheader("Highlights of an analysis performed using the farm_precip_project package")
