2454405644d28c8d89f24c53ef49813a39d105a6
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-11 12:50:37 -0700

    created tech doc qmd

--- src/farm_precip_project/__init__.py
+++ src/farm_precip_project/__init__.py
@@ -5,7 +5,8 @@ from .analysis import analysis_test
 from .merge_csvs import merge_csvs
 # import from all .py coding
 # all code should be in this folder
-# uv pip install -e
+# uv pip install -e .
+# uv run quarto preview
 
 all = ["txt_to_csv", "read_url_txt", "normalized_data", 
        "row_by_label", "extract_state_rows", "scrape_farm_data", 
