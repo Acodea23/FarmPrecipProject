76c00c59e42e60e4a616bc3ec723128d006d3be7
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-12 11:46:17 -0700

    finished analysis functions. fixed bug in documentation

--- src/farm_precip_project/analysis.py
+++ src/farm_precip_project/analysis.py
@@ -1,5 +1,46 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def analysis_test():
-    pass
 
+def remove_low_outliers(df, col_name, low_threshold):
+    df = df[df[col_name] > low_threshold]
+    return df
+
+
+def corr_and_plot(df, col1, col2, plot_file,n_digits):
+    correlation = round(df[col1].corr(df[col2]),n_digits)
+    
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df[col1], df[col2])
+    plt.title(f'Scatter plot of {col1} vs {col2}, Correlation: {correlation}')
+    plt.xlabel(col1)
+    plt.ylabel(col2)
+    plt.show()
+    plt.savefig(plot_file)
+
+    return correlation
+
+
+def make_scatter_w_cat(df, colx, coly, colcat, plot_file):
+    cat_order = sorted(df[colcat].unique())
+    palette = sns.color_palette("husl", len(cat_order))
+
+    sns.scatterplot(
+    data=df,
+    x=colx,
+    y=coly,
+    hue=colcat,
+    hue_order=cat_order,
+    palette=palette,
+    s=30
+    )
+
+    leg = plt.legend(title=colcat, bbox_to_anchor=(1.02, 1), 
+                     loc="upper left", borderaxespad=0.,
+                     ncol = 2)
+    for text in leg.get_texts():
+        text.set_fontsize(8)  # smaller labels
+
+    plt.show()
+    plt.savefig(plot_file)
