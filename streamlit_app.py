9392a53a518ed7b2b694c39f0617eb7564d2a683
Matthew Wilson <wilson.matthewg@gmail.com>
2025-12-15 10:06:37 -0700

    updated index to include streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+# https://farmprecipproject-overview.streamlit.app/
+
 st.title("State Farm Precipitation and Income Data Analysis")
 st.subheader("Highlights of an analysis performed using the farm_precip_project package")
